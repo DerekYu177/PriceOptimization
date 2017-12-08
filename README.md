c852914e0259e8ffc647c66ab2b738689a2a6482
DerekYu <derek-nis@hotmail.com>
2017-12-08 00:58:40 -0500

    starting the future from scratch

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ This is intended to be a small guide on a very simple web scraper for Electronic
 I have used this personally when building my first computer.
 It use to have support for NCIX, but as of last week (Dec 2017)...
 
+I will currently only support Newegg.ca for now.
+
 ##### Usage
 We call this via the command line interface with the following parameters:
 
@@ -12,9 +14,10 @@ We call this via the command line interface with the following parameters:
 
 `-p` Number of pages to scrape. By default this will be 1. Setting this value to 0 will scrape all pages.
 
-`-s` Output result to the screen (prettyTable)
+`-s` Output result to the screen (prettyTable). By default this will be `True`.
 
 `-f` Name of file to append output to. If the file has not been created, a warning will be made and the corresponding `.txt` file will be created.
+By default this will create `searches.txt` in the current directory.
 
 ###### Example usage
 > python3 price_finder "vega 64" -p 0 -s -f vega_result_searches.txt
--- price_optimizer_future.py
+++ price_optimizer_future.py
@@ -1,131 +1,48 @@
 #This future price_optimizer is planned to be more modular than current
-#by using objects instead of multi-dimensional lists, I hope we future-proof ourself. 
+#by using objects instead of multi-dimensional lists, I hope we future-proof ourself.
 
 '''
 Created on Feb 11, 2016
+Modified on Dec 7th, 2017
 
-@author: derekyu
+@author: derekyu177
 '''
 
-from lxml import html
-
-class Product:
-
-	""" Each product will be a object who's properties we will add to. """
-	def __init__(self, name):
-		#the name of the product is what is listed
-		self.name = name
-
-	def convertCurr():
-		#looks up the most up-to-date USD-CAD conversion to ensure accuracy
-		page = requests.get("https://www.google.ca/finance?q=usdcad")
-		tree = html.fromstring(page.content)
-		exchange_rate = tree.xpath('//span[@class="bld"]/text()')
-		usdcad = ((exchange_rate[0]).split())[0]
-		cadusd = (1/float(usdcad))
-		return [float(usdcad),float(cadusd)]
-
-	def addValue(self, native_currency, native_price):
-		self.native_currency = native_currency
-		self.native_price = native_price
-		usdcad = convertCurr()[0]
-		cadusd = convertCurr()[1]
-		if native_currency.upper() == "US":
-			self.nonnative_currency = "CA"
-			self.nonnative_price = usdcad*native_price
-		elif native_currency.upper() == "CA":
-			self.nonnative_currency = "US"
-			self.nonnative_price = cadusd*native_price
-
-	def addStore(self, store, link):
-		self.store = store
-		self.link = link
-
-	def addGenre(self, genre):
-		#genre is in which the product fits
-		#i.e. CPU, Motherboard
-		self.genre = genre
+import argparse
+import sys
 
 class Search:
-	""" Call upon this class when we want to do the web scraping """
+	def __init__(self, name, config):
+		self.name = name
+		self.config = config
 
-	#the site that we search
-	#Q: p1 only?
-	global base_search 
-	base_search = {
-	"microcenter" : "http://www.microcenter.com/search/search_results.aspx?Ntt=",
-	"ncix" : "http://search.ncix.com/search/ajaxsearch.cfm?q=",
-	"newegg" : "http://www.newegg.ca/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Description="
-	}
+def _print_configurations(args):
+	product = "Searching for " + args.product
+	verbose = "Verbosity level = " + str(args.verbose)
 
-	#the xpath we take to scrape the product names 
-	global product_xpath
-	product_xpath = {
-	"microcenter" : '//div[@class="normal"]/h2/a/text()',
-	"ncix" : '//span[@class="listing"]/a/text()',
-	"newegg" : '//span[@style="display:inline"]/text()'
-	}
+	plural = " pages" if args.pages > 1 else " page"
 
-	#the xpath to scrape the price from the site
-	#if there is a mismatch, this is where the problem will most likely be
-	#vulnerable to site HTML tag 
-	global price_xpath
-	price_xpath = {
-	"microcenter" : '//div[@class="price"]/span[@itemprop="price"]/text()',
-	"ncix" : '//font[@class="listing"]/strong/text()',
-	"newegg" : '//li[@class="price-current "]/strong/text() | //li[@class="price-current is-price-current-list"][1]/strong/text()'
-	}
+	pages_to_scrape = "Will scrape " + str(args.pages) + plural
 
-	global storeCurrency
-	storeCurrency = {
-	"microcenter" : "US"
-	"ncix" : "CA"
-	"newegg" : "CA"
+	output_to_screen = "Output to screen = " + str(args.output)
+	filename = "Appending to file: " + str(args.filename)
 
-	}
+	configurations = [product, verbose, pages_to_scrape, output_to_screen, filename]
 
-	global masterList
-	masterList = []
+	for configuration in configurations:
+		print(configuration)
 
-	def __init__(self, site, search_term):
-		term = search_term.replace(" ", "+")
-		self.site = "N/A"
-		for sites in base_search.keys():
-			if site == sites:
-				self.site = site
-				self.link = base_search[site] + term
- 				#the exception to the rule
-				if self.site == "newegg":
-					self.site = self.site.append(str(search_term) + "&N=-1&isNodeId=1")
+if __name__ == "__main__":
+	if len(sys.argv) == 1:
+		print("No product entered, exiting...")
+		sys.exit(0)
 
-	def scrape(self, site, target_site, target_search_term):
-		#this produces a product list and a price list. 
-		page = requests.get(target_site)
-		tree = html.fromstring(page.content)
-		self.product = tree.xpath(product_xpath[site])
-		self.price = tree.xpath(price_xpath[site])
-		self.col_length = max(len(self.product), len(self.price))
+	parser = argparse.ArgumentParser()
+	parser.add_argument('product', type=str, help='specify the product to search for')
+	parser.add_argument('-v', '--verbose', action='store_true', help='specify if verbose')
+	parser.add_argument('-p', '--pages', default=1, help='specify how many pages to scrape')
+	parser.add_argument('-s', '--output', default=True, help='specify if you desire output to console')
+	parser.add_argument('-f', '--filename', default='searches.txt', help='specify the file to append results to')
+	args = parser.parse_args()
 
-	def productList(self):
-		#here we expect to create a list of length col_length populated by product objects
-		for i in self.col_length
-			p = Product(self.product[i])
-			p.addValue()
-			masterList.append()
-		
-
-	def zipUp(self, product, product_name, product_price):
-		#appends the products with the appropriate product name and price list
-		pass
-
-class Visualizer:
-	"""We pass our list of product objects here where they will be visualized"""
-	def __init__(self):
-		pass
-
-s = Search("newegg", "Xeon E3")
-print s.site, s.link
-
-
-
-
+	_print_configurations(args)
