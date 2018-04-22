bb3be0338505a28ef9d45c582e0e15ab369ab87e
DerekYu <derek-nis@hotmail.com>
2018-04-21 23:52:09 -0400

    a more organized approach to dev

--- modules/pricer.py
+++ modules/pricer.py
@@ -1,7 +1,5 @@
-# from bs4 import BeautifulSoup as bs
 from urllib import request, parse
 from lxml import html
-import argparse
 import sys
 
 #This future price_optimizer is planned to be more modular than current
@@ -64,17 +62,3 @@ class Search:
 
         return products
 
-if __name__ == "__main__":
-	if len(sys.argv) == 1:
-		print("No product entered, exiting...")
-		sys.exit(0)
-
-	parser = argparse.ArgumentParser()
-	parser.add_argument('product', type=str, help='specify the product to search for')
-	parser.add_argument('-v', '--verbose', action='store_true', help='specify if verbose')
-	parser.add_argument('-p', '--pages', default=1, help='specify how many pages to scrape')
-	parser.add_argument('-s', '--output', default=True, help='specify if you desire output to console')
-	parser.add_argument('-f', '--filename', default='searches.txt', help='specify the file to append results to')
-	args = parser.parse_args()
-
-    Reporter(args).report
