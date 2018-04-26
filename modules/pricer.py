8448b894a7bf8b71bc7286c5678d0e175551a69a
DerekYu <derek-nis@hotmail.com>
2018-04-26 01:01:07 -0400

    search now finds the correct price and the name, what's left is to display it as an output

--- modules/pricer.py
+++ modules/pricer.py
@@ -1,6 +1,7 @@
 from urllib import request, parse
 from lxml import html
-import sys
+import tableprint as tp
+import pandas as pd
 
 #This future price_optimizer is planned to be more modular than current
 #by using objects instead of multi-dimensional lists, I hope we future-proof ourself.
@@ -11,54 +12,82 @@ Modified on Dec 7th, 2017
 
 @author: derekyu177
 '''
-class Reporter:
-    def report_configuration(self, args):
-        product = "Searching for " + args.product
-        verbose = "Verbosity level = " + str(args.verbose)
-
-        plural = " pages" if args.pages > 1 else " page"
-        pages_to_scrape = "Will scrape " + str(args.pages) + plural
-
-        output_to_screen = "Output to screen = " + str(args.output)
-        filename = "Appending to file: " + str(args.filename)
-
-        for configuration in [product, verbose, pages_to_scrape, output_to_screen, filename]:
-            print(configuration)
+class Reporter(object):
 
     def __init__(self, args):
         self.args = args
-        self.report_configuration(args)
-        products = Search(args.product).search(args.pages_to_scrape)
+        self.products = Searcher(args.product).search(args.pages)
+
+        if args.output:
+            self.write_to_screen()
+
+        write_to_file()
         #TODO: now that we have the products, we have to decide what to do with
         #them
 
-class Search:
-	def __init__(self, name):
-		self.name = name
+    def write_to_file(self):
+        import pdb; pdb.set_trace()
+
+    def write_to_screen(self):
+        tp.banner('Looking up: {}'.format(self.args.product))
+        import pdb; pdb.set_trace()
+        max_width = max(self.products)
+        tp.table(self.products, ['items', 'prices'], width)
+
+    def report(self):
+        pass
+
+class Searcher(object):
+    def __init__(self, name):
+        self.name = name
         self.site = self._search_url()
 
-	def _search_url(self):
-		site = "https://www.newegg.ca"
-		path = "/Product/ProductList.aspx"
-		query = "?"
-		parameters = {
-			"Submit": "ENE",
-			"Order": "BESTMATCH",
-			"Description": self.name,
-		}
-		return site + path + query + parse.urlencode(parameters)
+    def _search_url(self):
+        site = "https://www.newegg.ca"
+        path = "/Product/ProductList.aspx"
+        query = "?"
+        parameters = {
+            "Submit": "ENE",
+            "Order": "BESTMATCH",
+            "Description": self.name,
+        }
+        return site + path + query + parse.urlencode(parameters)
 
-    def _get_products(self):
-		raw_html = request.urlopen(self.site).read()
-		document = html.document_fromstring(raw_html)
-		element = document.xpath("//div[contains(@class, 'item-container')]")
+    def _get_products(self, number_of_pages):
+        #TODO: take number_of_pages into account
+        raw_html = request.urlopen(self.site).read()
+        document = html.document_fromstring(raw_html)
+        items = document.xpath("//a[contains(@class, 'item-title')]")
+        prices = document.xpath("//li[contains(@class, 'price-current')]")
+        return zip(items, prices)
 
-	def search(self):
-        product_elements = self._get_products()
+    def search(self, number_of_pages):
+        products = self._get_products(number_of_pages)
+        products = [Product(item, price) for item, price in products]
+        return
 
-        products = []
-        for element in product_elements:
-            products.append(Product(element))
+class Product(object):
 
-        return products
+    def clean_price(self, price):
+        price_information = [e.text for e in price]
+        useful_information = price_information[1:-1]
+        # pricing is the first two, the last is the offers
+        price = ''.join(useful_information[0:1])
 
+        try:
+            offers = useful_information[2]
+        except IndexError as exception: # there was no offer information
+            offers = None
+
+        return price
+
+    def __repr__(self):
+        return {
+            'Item': str(self.item),
+            'Price': float(self.price),
+            'Item Name Length': len(self.item)
+        }
+
+    def __init__(self, item, price):
+        self.item = item.text_content()
+        self.price = self.clean_price(price)
