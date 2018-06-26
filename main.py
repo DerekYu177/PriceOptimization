9eccbd8268cfbb334a84e1f2395092a1e1795c1b
DerekYu <derek-nis@hotmail.com>
2018-06-26 10:45:14 -0400

    Using the DynamicTablePrint via a hack

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sys
 import argparse
 
+# fukin hack
+sys.path.append('../dynamictableprint')
+
 from modules.pricer import Reporter
 
 '''
@@ -35,12 +38,24 @@ if __name__ == "__main__":
         sys.exit(0)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('product', type=str, help='specify the product to search for')
-    parser.add_argument('-v', '--verbose', action='store_true', help='specify if verbose')
-    parser.add_argument('-p', '--pages', type=int, default=1, help='specify how many pages to scrape')
-    parser.add_argument('-a', '--only_available', action='store_true', help='shows only items in stock')
-    parser.add_argument('-s', '--output', default=True, help='specify if you desire output to console')
-    parser.add_argument('-f', '--filename', default='searches.txt', help='specify the file to append results to')
+    parser.add_argument(
+        'product', type=str,
+        help='specify the product to search for')
+    parser.add_argument(
+        '-v', '--verbose', action='store_true',
+        help='specify if verbose')
+    parser.add_argument(
+        '-p', '--pages', type=int, default=1,
+        help='specify how many pages to scrape, "0" for all available pages')
+    parser.add_argument(
+        '-a', '--only_available', action='store_true',
+        help='shows only items in stock')
+    parser.add_argument(
+        '-s', '--output', default=True,
+        help='specify if you desire output to console')
+    parser.add_argument(
+        '-f', '--filename', default='searches.txt',
+        help='specify the file to append results to')
     args = parser.parse_args()
 
     if args.verbose:
--- modules/pricer.py
+++ modules/pricer.py
@@ -1,6 +1,7 @@
 import os
 import copy
 import tableprint as tp
+from dynamictableprint import DynamicTablePrint as dtp
 
 from .product import Searcher
 
@@ -21,76 +22,23 @@ class Reporter(object):
         self.products = searcher.search(args.pages)
         self.product_info = searcher.product_info()
 
-        if args.only_available:
-            self.filter_stock()
-
-        if args.output:
-            self.write_to_screen()
-
-        self.write_to_file()
-        #TODO: now that we have the products, we have to decide what to do with
-        #them
-
     def filter_stock(self):
         self.products = self.products[self.products['in_stock'] == True]
-
 
     def write_to_file(self):
         pass
 
     def write_to_screen(self):
-        screen_width, widths = self._fit_screen()
-
-        tp.banner(
-            'Looking up: {}, {} pages of {}'.format(
-                self.args.product,
-                self.args.pages,
-                self.product_info["total_number_pages"]
-            ),
-            width=screen_width
-        )
-
-        if self.products.empty:
-            tp.banner(
-                'ERROR: No results',
-                width=screen_width
-            )
-        else:
-            tp.dataframe(self.products, width=widths)
-
-    def _fit_screen(self):
-        screen_width = os.get_terminal_size(0)[0]-2
-
-        # we get the maximum width for each category
-        # that is not 'item'
-
-        columns = self.products.columns.values.tolist()
-
-        fixed_columns = copy.copy(columns)
-        fixed_columns.remove('item')
-        column_widths = { column:self._max_width_for(column) for column in
-                fixed_columns }
-
-        item_width = screen_width - sum(column_widths.values()) - 8 # padding
-        column_widths.update({ 'item':item_width })
-
-        self.products['item'] = self.products['item'] \
-                .apply(lambda x: self._item_trucation(item_width, x))
-
-        widths = tuple([column_widths[column] for column in columns])
-        return screen_width, widths
-
-    def _item_trucation(self, width, line):
-        return line if len(line) < width else line[:width-3] + '...'
-
-    def _max_width_for(self, item):
-        product_width = self.products[item] \
-                .apply(lambda x: len(str(x))) \
-                .max()
-
-        name_width = len(str(item))
-        return max(product_width, name_width)
+        dtp(self.products).write_to_screen()
 
     def report(self):
-        pass
+        if self.args.only_available:
+            self.filter_stock()
 
+        if self.args.output:
+            self.write_to_screen()
+
+        self.write_to_file()
+
+        #TODO: now that we have the products, we have to decide what to do with
+        #them
